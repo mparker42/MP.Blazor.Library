7185a57e59fecd2ab0776e92cf5c758b34b865b8
mparker42 <mparker42@hotmail.co.uk>
2021-11-27 11:32:21 +0000

    Add BlazorApplicationInsights package

--- MP.Blazor.Library/Dependencies.cs
+++ MP.Blazor.Library/Dependencies.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BlazorApplicationInsights;
+using Microsoft.Extensions.DependencyInjection;
 using MP.Blazor.Library.Models;
 using MP.Blazor.Library.Services;
 using MP.Blazor.Library.Services.Interfaces;
@@ -14,6 +15,7 @@ namespace MP.Blazor.Library
             services.AddHttpClient();
             services.AddSingleton(siteDescription);
             services.AddSingleton<ITranslationService, TranslationService>();
+            services.AddBlazorApplicationInsights();
 
             return services;
         }
